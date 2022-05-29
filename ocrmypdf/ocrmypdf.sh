9e533a9d1d7b24fb82033f347f1369230de96066
Home Assistant <hamelbpc@gmail.com>
2022-05-29 15:29:59 +1000

    made changes

---
+++ ocrmypdf/ocrmypdf.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -x
+
+function ocrmypdf()
+{
+docker run --rm -i  --user "$(id -u):$(id -g)" --workdir /data -v "$PWD/output:/data" jbarlow83/ocrmypdf --sidecar $3 - - <$1 >$2
+}
+
+docker run --rm -i jbarlow83/ocrmypdf --version
+
+for file in input/*.pdf
+do
+echo "Processing file: $file"
+outputfile=$(echo $file|sed 's/^input\//output\//')
+outputocr=$(echo $file|sed -e 's/^input\///' -e 's/\.pdf/\.txt/')
+#docker run --rm -i jbarlow83/ocrmypdf  - - <$file >$outputfile 2>/dev/null
+ocrmypdf $file $outputfile $outputocr 
+done
+
