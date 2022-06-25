d325f27b8f79af3131cb62de279827f7caeb88e9
Home Assistant <hamelbpc@gmail.com>
2022-06-25 11:44:08 +1000

    made changes

--- portainer/generateadminpassword.sh
+++ portainer/generateadminpassword.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-newpassword=$(docker run --rm httpd:alpine htpasswd -nbB admin 'Passwordp@ssword' | cut -d ":" -f 2)
-newpassword=$(echo $newpassword|sed 's/\$/\$\$/g')
-echo $newpassword
+docker run --rm httpd:alpine htpasswd -nbB admin 'password' | cut -d ":" -f 2 |sed 's/\$/\$\$/g' 
+
